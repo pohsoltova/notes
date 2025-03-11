d817ec80775cb9048e36dcba2b7eaae159c932ba
unknown <vilma.pohsoltova@infis.cz>
2025-03-11 09:15:44 +0100

    2

---
+++ notes.py
@@ -0,0 +1,15 @@
+# pohsoltova 
+
+input("napis neco")
+# priklad nacteni od uzivatele
+
+cislo = "meow"
+int(cislo)
+# priklad pretypovani na integer
+
+if cislo >= 5:
+    print("cislo je fajn")
+elif cislo <5:
+    print("cislo je mensi nez 5")
+else:
+    print("neni cislo")
