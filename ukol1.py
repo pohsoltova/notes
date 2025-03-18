48739393441d542705d66d141af05a32297d9335
unknown <vilma.pohsoltova@infis.cz>
2025-03-18 09:24:25 +0100

    1

---
+++ ukol1.py
@@ -0,0 +1,18 @@
+rok = 2025
+text = "vesele velikonoce"
+cislo = 52168486
+# f string
+print(f"ahoj {text} {rok}")
+# rozdelovac cisel
+print(f"{cislo:,}")
+
+# vypsat pocet pismen
+print(f"{text:.6}")
+
+#desetinna cisla
+print(f"{cislo:.3f}")
+
+#prevody jednotek
+hodnota = 155
+print(f"{hodnota:b}")
+print(f"{hodnota:x}")
