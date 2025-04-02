54965285886ee73e35d0f23891f0d7ae8e6f4a63
unknown <vilma.pohsoltova@infis.cz>
2025-04-02 13:20:15 +0200

    1

---
+++ ukolPole.py
@@ -0,0 +1,25 @@
+opakovani = "ano"
+
+while opakovani == "ano":
+    cislo1 = int(input())
+    cislo2 = int(input())
+    znamenko = (input())
+    if znamenko == "+":
+        vysledek = cislo1-cislo2
+        cisla.append(vysledek)
+    elif znamenko == "-":
+        vysledek = cislo1-cislo2
+        cisla.append(vysledek)
+    elif znamenko == "*":
+        vysledek = cislo1*cislo2
+        cisla.append(vysledek)
+    elif znamenko == "/":
+        vysledek = cislo1/cislo2
+        cisla.append(vysledek)
+    import random
+    cisla = []
+    cisla.append(vysledek)
+    random.choice(cisla)
+    
+    
+
