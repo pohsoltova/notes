0d70022c7ac3b6b268c454d803c52097cd4e0a05
unknown <vilma.pohsoltova@infis.cz>
2025-05-06 09:24:29 +0200

    1

---
+++ hra.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+# Získání aktuálního času
+now = datetime.now()
+hour = now.hour  # hodina ve formátu 0–23
+
+# Podmínka podle aktuální hodiny
+if 
+    if 18 <= hour < 6:
+        print("získáváte rare exotic item!!")
+    else:
+        print("získáváte obyčejnou výhru!!!")
+
+
+#### vyhra se bude menit podle aktualniho casu - pokud bude 18:00 a vys tak bude vyhra zajimavejsi,  pokud bude 6:00 a vys tak bude obycejna
