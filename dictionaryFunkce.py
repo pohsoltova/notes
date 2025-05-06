7c7762d0d59f178dab6b5e8328caa29cad52cbd2
pohsoltova <vilma.pohsoltova@infis.cz>
2025-05-06 21:09:28 +0200

    4

--- dictionaryFunkce.py
+++ dictionaryFunkce.py
@@ -2,17 +2,14 @@
 def vytvor_utulek():
     return {}
 
-# 2. Přidání zvířete do útulku
 def pridej_zvire(utulek, jmeno, druh, vek):
     utulek[jmeno] = {"druh": druh, "vek": vek}
 
-# 3. Výpis všech zvířat
 def vypis_zvirata(utulek):
     for jmeno, info in utulek.items():
         print(f"{jmeno} je {info['druh']} a je mu {info['vek']} let.")
 
 
-# 5. Zavolání funkcí pro ověření
 if __name__ == "__main__":
     utulek = vytvor_utulek()
     pridej_zvire(utulek, "Míca", "kočka", 3)
